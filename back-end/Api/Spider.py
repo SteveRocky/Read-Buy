255f30b1363dca4148e0519e779d9f0b223bc295
Rocky <230604910@qq.com>
2019-11-18 20:12:49 +0800

    complete regx

--- back-end/Api/Spider.py
+++ back-end/Api/Spider.py
@@ -2,8 +2,32 @@ import requests
 import re
 
 
-url = "http://www.zongheng.com/"
-response = requests.get(url)
-response.encoding = 'utf-8'
-with open("../Resourse/html.txt", 'w', encoding='utf-8') as fp:
-    fp.write(response.text)
+# url = "http://book.douban.com/latest?icn=index-latestbook-all"
+# headers = {
+#     'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Mobile Safari/537.36'
+# }
+# response = requests.get(url, headers=headers)
+# response.encoding = 'utf-8'
+# print(response.text)
+with open("../Resourse/html.txt", 'r', encoding='utf-8') as fp:
+    response = fp.read()
+
+result = re.findall("<li>(.*?)</li>", response, re.S)
+
+
+def judge(result):
+    photo_url = re.findall('<img src="(.*?)"/>', result)
+    if photo_url == []:
+        return False
+    try:
+        book_url = re.search(r'https://(.*?)"', result).group(0)
+    except AttributeError:
+        pass
+    name = re.findall(str(book_url)+">(.*?)</a>", result)[1]
+    author = re.findall(r'color-gray">\n(.*?)\n', result)
+    detail = re.findall(r'<p.*?>\n\s(.*?)\n', result)[2]
+    print(photo_url, book_url, name, author, detail)
+
+
+for i in result:
+    judge(i)
--- front-end/js/homePage.js
+++ front-end/js/homePage.js
@@ -1,38 +1,52 @@
 $(function(){
     // 轮播图自动播放
     plant_play();
-    // showAuto()
+    move();
 })
-// cont[0]放的是轮播图循环数
-var cont = [1];
+
 
 function plant_play(){
     for(let i = 0; i<$(".plant li").length; i++){
+        //获取父元素的宽度，即屏幕宽度，实现不同平台都能自适应屏幕大小小
+        let width = $(".plant").innerWidth();
         let str = "url(D:/Read&Listen/front-end/images/book" + i + ".jpg)"
-        $(".plant li").eq(i).css("background-image", str)
+        $(".plant li").eq(i).css("background-image", str);
+        $(".plant li").eq(i).css("left", String(i*width)+"px")
     }
-    let cont = 1
-
-    var playTime = setInterval("showAuto()", 1000);
-
+    var playTime = setInterval("showAuto()", 5000);
 }
 
 
-
 function showAuto(){
-    if(cont[0]==5){
-        cont[0]=0;
+    let width = $(".plant").innerWidth();
+    for(let i=0; i<3; i++){
+        let left = $('.plant li').eq(i).offset().left;
+        if((left-width)<-width){
+            $('.plant li').eq(i).css({
+                'left': String(width*1)+'px',
+                "zIndex" : "-2",
+                'transition': 'left 0s',
+        }); 
+        }else{
+            $('.plant li').eq(i).css('left', );
+            $('.plant li').eq(i).css({
+                'left': String(Math.round(left-width))+'px',
+                'transition': 'left 1s',
+            })
+        }
     }
-    for(let i=0; i<$(".plant li").length; i++){
-        // console.log(i);
-        let left = $(".plant").eq(i).offset();
-        console.log(left);
-    }
-    let width = $(".plant").eq(i).innerWidth();
-    $(".plant ul").css({
-        "left": String(-width*cont[0])+"px",
-        "transition": "left 0.5s"
-    });
-    
-    cont[0]+=1;
+}
+
+function move(){
+    $(".type").mousedown(function(e){
+        console.log('down');
+        $(this).mousemove(function(e){
+            let pageX = e.pageX;
+            let pageY = e.pageY;
+            console.log(pageX, pageY);
+            $(this).mouseup(function(e){
+                
+            })
+        })
+    })
 }
