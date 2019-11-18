dac7c2618429ffaa30ab25dbb799800fda55ea6e
Rocky <230604910@qq.com>
2019-11-19 00:38:34 +0800

    spider photo

--- back-end/Api/Spider.py
+++ back-end/Api/Spider.py
@@ -1,33 +1,56 @@
 import requests
 import re
 
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Mobile Safari/537.36'
+}
 
-# url = "http://book.douban.com/latest?icn=index-latestbook-all"
-# headers = {
-#     'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Mobile Safari/537.36'
-# }
-# response = requests.get(url, headers=headers)
-# response.encoding = 'utf-8'
-# print(response.text)
+
+# url请求函数
+def get_data(url):
+    response = requests.get(url, headers=headers)
+    response.encoding = 'utf-8'
+    return response
+
+
 with open("../Resourse/html.txt", 'r', encoding='utf-8') as fp:
-    response = fp.read()
-
-result = re.findall("<li>(.*?)</li>", response, re.S)
+    html = fp.read()
+results = re.findall("<li>(.*?)</li>", html, re.S)
 
 
 def judge(result):
     photo_url = re.findall('<img src="(.*?)"/>', result)
+
     if photo_url == []:
-        return False
-    try:
-        book_url = re.search(r'https://(.*?)"', result).group(0)
-    except AttributeError:
-        pass
-    name = re.findall(str(book_url)+">(.*?)</a>", result)[1]
-    author = re.findall(r'color-gray">\n(.*?)\n', result)
-    detail = re.findall(r'<p.*?>\n\s(.*?)\n', result)[2]
-    print(photo_url, book_url, name, author, detail)
+        print('url无效')
+    else:
+
+        try:
+            book_url = re.search(r'https://(.*?)"', result).group(0)
+        except AttributeError:
+            pass
+        name = re.findall(str(book_url)+">(.*?)</a>", result)[1]
+        author = re.findall(r'color-gray">\n(.*?)\n', result)
+        detail = re.findall(r'<p.*?>\n\s(.*?)\n', result)[2]
+        print(photo_url[0], book_url, name, author[0].replace(" ", ""), detail.replace(" ", ""))
+        date = {
+            "photo_url": photo_url[0],
+            "book_url": book_url,
+            "name": name,
+            "author": author[0].replace(" ", ""),
+            "detail": detail.replace(" ", "")
+        }
+        # 照片的二进制数据
+        photo_base = get_data(photo_url[0]).content
+        with open("../Resourse/Images/"+name+".jpg", 'wb') as ph:
+            ph.write(photo_base)
+        with open("../Resourse/Images/content.txt", 'a', encoding='utf-8') as cont:
+            cont.write(str(date))
 
 
-for i in result:
-    judge(i)
+for result in results:
+    judge(result)
+
+
+
+
