7099c57766de2e90daa14b8baeace31c937964fe
Rocky <230604910@qq.com>
2019-11-19 20:09:35 +0800

    cloud store images

---
+++ back-end/Api/Image_store.py
@@ -0,0 +1,26 @@
+from qiniu import Auth, put_file, etag, put_data
+import qiniu.config
+
+
+# 需要填写你的 Access Key 和 Secret Key
+access_key = 'INwRUsgA7boGEi6fbM8kQU4d_ep2N87XNrTul946'
+secret_key = 'UfpLl_dN6OWuATgVjrDyxT4kwlanKq_iuCnRCFw4'
+
+# 构建鉴权对象
+q = Auth(access_key, secret_key)
+
+# 要上传的空间
+bucket_name = 'read-buy'
+
+
+def image_store(file_data):
+    # 生成上传 Token，可以指定过期时间等
+    token = q.upload_token(bucket_name, None, 3600)
+
+    # 要上传文件的本地路径
+    # localfile = './sync/bbb.jpg'
+
+    # put_data上传二进制图片
+    ret, info = put_data(token, None, file_data)
+    return ret["key"]
+
--- back-end/Api/Spider.py
+++ back-end/Api/Spider.py
@@ -1,5 +1,6 @@
 import requests
 import re
+from Image_store import image_store
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Mobile Safari/537.36'
@@ -32,18 +33,19 @@ def judge(result):
         name = re.findall(str(book_url)+">(.*?)</a>", result)[1]
         author = re.findall(r'color-gray">\n(.*?)\n', result)
         detail = re.findall(r'<p.*?>\n\s(.*?)\n', result)[2]
-        print(photo_url[0], book_url, name, author[0].replace(" ", ""), detail.replace(" ", ""))
+        photo_data = get_data(photo_url[0]).content
+        cloud_name = image_store(photo_data)
         date = {
             "photo_url": photo_url[0],
             "book_url": book_url,
             "name": name,
+            "cloud_url": "http://q17qc0xmu.bkt.clouddn.com/"+cloud_name,
             "author": author[0].replace(" ", ""),
             "detail": detail.replace(" ", "")
+
         }
-        # 照片的二进制数据
-        photo_base = get_data(photo_url[0]).content
-        with open("../Resourse/Images/"+name+".jpg", 'wb') as ph:
-            ph.write(photo_base)
+        print(date['cloud_url'])
+        # 相当于把数据存入数据库,之后需要把这些数据存储到mysql、redis中
         with open("../Resourse/Images/content.txt", 'a', encoding='utf-8') as cont:
             cont.write(str(date))
 
